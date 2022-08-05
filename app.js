ecf2c3ac868c3c06d85dc4f8f856c8474834a437
Huncho Head <jeremyloki93@gmail.com>
2022-08-05 05:31:18 +0300

    add start script

--- app.js
+++ app.js
@@ -172,6 +172,7 @@ app.use((err, req, res, next) => {
     console.log(err.stack)
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
